f379213f36ff949d9380a5b04a30ec15bc385db6
wbmccurry20 <wbmccurry20@gmail.com>
2021-12-16 16:01:38 -0500

    First commit including Terraform/Azure/Docker training

---
+++ Terraform/terraform_azure/main.tf
@@ -0,0 +1,34 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "eastus2"
+  
+  tags = {
+    Environment = "Getting Started with Terraform"
+    Team = "DevOps"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name          = "myTFVnet"
+  address_space = ["10.0.0.0/16"]
+  location      = "eastus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
