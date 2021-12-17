524b054f70ff1444ff3f5b5dea70ac837f61c707
Dylan Silinski <dylan.silinski@gmail.com>
2021-12-16 20:54:25 -0500

    add gcp terrafrom

---
+++ Terraform/terraform_gcp/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.key_path)
+
+  project = var.gcp_project_id
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
