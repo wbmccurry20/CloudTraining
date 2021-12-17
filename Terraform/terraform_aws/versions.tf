cc8e48b2b984b523018cfa4996a1354bc6994a62
William McCurry <43455067+wbmccurry20@users.noreply.github.com>
2021-12-17 00:52:23 -0500

    Update versions.tf

--- Terraform/terraform_aws/versions.tf
+++ Terraform/terraform_aws/versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.28.0"
+      version = "1.1.1"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.0.0"
+      version = "1.1.1"
     }
   }
 
