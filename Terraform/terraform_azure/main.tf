01bd4d7dd35501b7fb6ba4a3a64e15e7e0f55eaf
wbmccurry20 <wbmccurry20@gmail.com>
2021-12-19 22:06:37 -0500

    update tag in main.tf

--- Terraform/terraform_azure/main.tf
+++ Terraform/terraform_azure/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_resource_group" "rg" {
   location = "eastus2"
   
   tags = {
-    Environment = "Getting Started with Terraform"
+    Environment = "tf_azure"
     Team = "DevOps"
   }
 }
