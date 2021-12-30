3cfe1f0684cb952fdfb27c74a13286ff1807bbd0
Dylan Silinski <dylan.silinski@gmail.com>
2021-12-30 08:28:02 -0500

    initial push

---
+++ Terraform/terraform_azure/az_backend/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  cloud {
+    organization = "dw_cloud"
+    workspaces {
+      name = "terraform_azure"
+    }
+  }
+}
+
+## remove provider block once running from root module
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "eastus2"
+
+  tags = {
+    Environment = "Getting Started with Terraform"
+    Team        = "DevOps"
+  }
+}
+
+resource "azurerm_storage_account" "DW_storage" {
+  name                     = "dwstorage"
+  resource_group_name      = var.resource_group_name
+  location                 = "eastus2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "dwbackendcontainer"
+  storage_account_name  = azurerm_storage_account.DW_storage.name
+  container_access_type = "blob"
+}
