99c168f65a3f9e1c0b7cc42465c020d6d9252477
Dylan Silinski <dylan.silinski@gmail.com>
2021-12-31 09:16:25 -0500

    providers

--- Terraform/terraform_azure/az_backend/main.tf
+++ Terraform/terraform_azure/az_backend/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.90.0"
-    }
-  }
+  # required_providers {
+  #   azurerm = {
+  #     source  = "hashicorp/azurerm"
+  #     version = "~> 2.90.0"
+  #   }
+  # }
   cloud {
     organization = "dw_cloud"
     workspaces {
