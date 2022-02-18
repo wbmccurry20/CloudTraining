bd291da582fb2932fe89c3f2ca3909c9863326f7
wbmccurry20 <wbmccurry20@gmail.com>
2022-02-17 20:38:15 -0500

    harbr takehome

---
+++ Terraform/harbr/main.tf
@@ -0,0 +1,63 @@
+# Configure the AWS provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  cloud {
+    organization = "dw_cloud"
+    workspaces {
+      name = "harbr_takehome"
+   }
+  }
+ }
+
+provider "aws" {
+  alias = "PROD"
+  region = us-east-1
+}
+
+provider "aws" {
+  alias = "NONPROD"
+  region = us-west-1
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ubuntu" {
+  provider = "PROD"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_instance" "ubuntu" {
+  provider = "NONPROD"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+
---
+++ Terraform/harbr/variables.tf
@@ -0,0 +1,13 @@
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  type = "map"
+  description = "EC2 instance name"
+  default     = {
+	"PROD" = "PROD"
+	"NONPROD" = "NONPROD"
+}
+
