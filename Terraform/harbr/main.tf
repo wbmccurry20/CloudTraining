180f02c85bbe71f1d361342b41f6ba5232824d9c
William McCurry <43455067+wbmccurry20@users.noreply.github.com>
2022-02-17 21:15:00 -0500

    Update main.tf

--- Terraform/harbr/main.tf
+++ Terraform/harbr/main.tf
@@ -32,7 +32,24 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["self"]
+}
+
+data "aws_ami" "ubuntu" {
+  provider = aws.nonprod
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["self"]
 }
 
 resource "aws_instance" "ubuntu_prod" {
