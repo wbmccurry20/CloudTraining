3652a4ba54e85c0b28cf129307af492c074ee9c3
wbmccurry20 <wbmccurry20@gmail.com>
2022-05-27 11:12:43 -0400

    github actions testing

---
+++ Terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+       backend "s3" {
+       bucket = "[gh_actions_bucket]"
+       key    = "[Remote_State_S3_Bucket_Key]"
+       region = "east-us-1"
+   }
+}
+
+provider "aws" {
+  version = "~>3.0"
+  region  = "east-us-1"
+}
--- Terraform/terraform_aws/main.tf
+++ Terraform/terraform_aws/main.tf
@@ -2,27 +2,16 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "aws_s3_bucket" "b" {
+  bucket = "gh_actions_bucket"
 
   tags = {
-    Name = var.instance_name
+    Name        = "gh_bucket"
+    Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
 }
